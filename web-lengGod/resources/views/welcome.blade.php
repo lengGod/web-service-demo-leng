97590af1323a7c54a329ae2f31b4936502853e5b
lengGod <galangsamudro264@gmail.com>
2024-10-31 23:50:17 +0700

    menambahkan link navbar baru

--- web-lengGod/resources/views/welcome.blade.php
+++ web-lengGod/resources/views/welcome.blade.php
@@ -18,6 +18,8 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
                 <li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
                 <li class="nav-item"><a class="nav-link" href="/portfolio">Portfolio</a></li> 
                 <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li> 
+                <li class="nav-item"><a class="nav-link" href="/contact">Profile</a></li> 
+                <li class="nav-item"><a class="nav-link" href="/contact">Dashboard</a></li> 
             </ul> 
         </div> 
     </nav> 
